f60c0ab7258dc8a943c78898d319cab118e66c84
Ayushmaan <118063648+ayushmaan2207@users.noreply.github.com>
2024-01-12 21:54:02 +0530

    all

--- 1ST YEAR/C PROGRAMING/C CLASS/Q1.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    char a[20],b[20];
-    printf("input: ");
-    scanf("%s %s",&a,&b);
-    printf("output: %s %s",a,b);
-}
--- 1ST YEAR/C PROGRAMING/C YT/10_FOR_LOOP.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    int i;
-    int luckynum[]={4,8,15,16,23,42};
-    for(i=0;i<6;i++){
-        printf("%d\n",luckynum[i]);
-    }
-} 
