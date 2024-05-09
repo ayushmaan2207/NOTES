bed400187f054022d0b7e37b29a8533c0cf5f19b
Ayushmaan <118063648+ayushmaan2207@users.noreply.github.com>
2024-05-09 22:39:21 +0530

    v

--- 2ND YEAR/C++/C++ YT/2_DATA_TYPES.cpp
+++ 2ND YEAR/C++/C++ YT/2_DATA_TYPES.cpp
@@ -8,12 +8,13 @@ int main(){
     cout<<_sa45<<endl;
 
     int MarksInMaths=83;    // camelcase Notation
-    cout<<"marks in maths is "<<marksInMaths<<endl;
-    return 0;
+    cout<<"marks in maths is "<<MarksInMaths<<endl;
 
     float score=45.2;
     double score2= 45.3;
     long double const score3=45.4;
 
     string g ="string";
+    cout<<g;
+    return 0;
 }
