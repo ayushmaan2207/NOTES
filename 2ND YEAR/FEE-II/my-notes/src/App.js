30b3b8d1a279b2d48738cb91e8868330c4039f98
Ayushmaan <118063648+ayushmaan2207@users.noreply.github.com>
2024-06-19 20:34:09 +0530

    n

--- 2ND YEAR/FEE-II/my-notes/src/App.js
+++ 2ND YEAR/FEE-II/my-notes/src/App.js
@@ -1,33 +1,37 @@
-import './App.css';
-import React,{useState} from 'react';
-import data from './data';
-import AddNotes from './components/AddNotes';
-import Notes from './components/Notes';
-
+import "./App.css";
+import React, { useState } from "react";
+import data from "./data";
+import AddNotes from "./components/AddNotes";
+import Notes from "./components/Notes";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  const [notes,setNotes] = useState(data);
+  const [notes, setNotes] = useState(data);
 
-  function addnote(text){
-    // const obj={
-    //   desc:text,
-    // }
-    // const newNotes = [...notes, obj];
-    // setNotes(newNotes);
-    
-    setNotes(prevNotes => [...prevNotes, { desc: text }]);
+  function addnote(text) {
+    if (text.length > 0) {
+      // const obj={
+      //   desc:text,
+      // }
+      // const newNotes = [...notes, obj];
+      // setNotes(newNotes);
+      toast.success("Note Added");
+      setNotes((prevNotes) => [...prevNotes, { desc: text }]);
+    }
   }
 
-  function remove(desc){
-    const newnotes= notes.filter(notes=>notes.desc!== desc);
+  function remove(desc) {
+    const newnotes = notes.filter((notes) => notes.desc !== desc);
+    toast.error("Note Deleted");
     setNotes(newnotes);
-  }  
+  }
 
   return (
     <div className="App">
       <div className="heading">MY-NOTES</div>
       <AddNotes addn={addnote}></AddNotes>
-      <Notes notes={notes} del={remove}/>
+      <Notes notes={notes} del={remove} />
     </div>
   );
 }
--- 2ND YEAR/FEE-II/my-notes/src/index.js
+++ 2ND YEAR/FEE-II/my-notes/src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <div>
         <App />
-        <ToastContainer/>
+        <ToastContainer />
     </div>
 );
 
