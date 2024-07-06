3ef8e14f1c77c640fd7da31182a77b198df86159
Ayushmaan <118063648+ayushmaan2207@users.noreply.github.com>
2024-07-06 23:09:44 +0530

    b

--- 1ST YEAR/C PROGRAMING/C CLASS/Q3.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int r,a,c;
-    printf("Enter radius:");
-    scanf("%d",&r);
-    a=3.14*(r*r);
-    c=2*(3.14*r);
-    printf("circumference: %d\narea: %d",c,a);
-}
--- 1ST YEAR/C PROGRAMING/C CLASS/Q6b.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main(){
-    int a,b;
-    printf("Enter 2 number a & b: ");
-    scanf("%d %d",&a,&b);
-    a=(a+b)-(b=a);
-    printf("number a & b: %d %d",a,b);
-}
