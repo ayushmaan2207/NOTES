3d4c57e0c4b072ea35c5bcd41a04d53b23b6855c
Ayushmaan <118063648+ayushmaan2207@users.noreply.github.com>
2024-07-06 16:36:40 +0530

    nn

--- 2ND YEAR/FEE-II/my-notes/src/data.js
+++ 2ND YEAR/FEE-II/my-notes/src/data.js
@@ -1,11 +1,41 @@
 const data =[
     {
         id:1,
-        tag: "Improtant",
+        tag: "Important",
         desc: "Learn React, MongoDB and Improve your LinkedIn account.",
     },
     {
         id:2,
+        tag: "Normal",
+        desc: "Stay positive and be productive.",
+    },
+    {
+        id:3,
+        tag: "Important",
+        desc: "Learn React, MongoDB and Improve your LinkedIn account.",
+    },
+    {
+        id:4,
+        tag: "Normal",
+        desc: "Stay positive and be productive.",
+    },
+    {
+        id:5,
+        tag: "Urgent",
+        desc: "Learn React, MongoDB and Improve your LinkedIn account.",
+    },
+    {
+        id:6,
+        tag: "Urgent",
+        desc: "Stay positive and be productive.",
+    },
+    {
+        id:7,
+        tag: "Normal",
+        desc: "Learn React, MongoDB and Improve your LinkedIn account.",
+    },
+    {
+        id:8,
         tag: "Urgent",
         desc: "Stay positive and be productive.",
     },
