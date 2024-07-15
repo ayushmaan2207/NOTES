4dc6998ef443cbdfde5b60d5bf73e45596e9d332
Ayushmaan <118063648+ayushmaan2207@users.noreply.github.com>
2024-07-15 23:15:26 +0530

    jhbhjd

--- 2ND YEAR/FEE-II/React T/02.Intermediate/src/App.js
+++ 2ND YEAR/FEE-II/React T/02.Intermediate/src/App.js
@@ -1,25 +1,48 @@
-import './App.css';
-import Form from './components/Form'
-import Counter from './components/Counter'
-import {Route,Routes,NavLink} from 'react-router-dom'
+import "./App.css";
+import Form from "./components/Form";
+import Counter from "./components/Counter";
+import { Route, Routes, NavLink, Link } from "react-router-dom";
 // navlink active class lgayega
+import { useState } from "react";
 
 function App() {
+  const [isLoggedIn, setisLoggedIn] = useState(false);
+
   return (
     <div className="App">
+
+      
       <nav>
         <ul>
           <li>
-            <NavLink to="/Form" > Form</NavLink>
+            <NavLink to="/"> Home</NavLink>
           </li>
           <li>
-            <NavLink to="/Counter" > Counter</NavLink>
+            <NavLink to="/Form"> Form</NavLink>
           </li>
+          <li>
+            <NavLink to="/Counter"> Counter</NavLink>
+          </li>
+
+          {!isLoggedIn && ( //jab logged in nhi hoga
+            <li>
+              <Link to="/Login">Log In</Link>
+            </li>
+          )}
+          {isLoggedIn && ( //jab logged in hoga
+            <li>
+              <Link to="/">Log Out</Link>
+            </li>
+          )}
         </ul>
       </nav>
+
+
       <Routes>
-        <Route path="/Form" element={<Form/>}/>
-        <Route path="/Counter" element={<Counter/>}/>
+        <Route path="/" element={<div>welcome to home page</div>} />
+        <Route path="/Form" element={<Form />} />
+        <Route path="/Counter" element={<Counter />} />
+        <Route path="*"element={<img src="emptystate.png" alt="Page Not Found" />} />
       </Routes>
     </div>
   );
