74b3efce5d41f04b4658dd78eba5c8737101eaec
Ayushmaan <118063648+ayushmaan2207@users.noreply.github.com>
2024-07-31 14:58:25 +0530

    GHJK

---
+++ 3RD YEAR/BE/02_FOLDER_STRUCTURE/index.js
@@ -0,0 +1,11 @@
+// npm init -y
+// npm i express
+
+// npm i nodemon
+
+//  require statement imports 
+const express =  require("express");
+const app=express();
+app.listen(3000,()=>{
+    console.log("server start hogya");
+})
