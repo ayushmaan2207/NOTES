e7200658f994cbd0d05367e18421669676a890c7
Ayushmaan <118063648+ayushmaan2207@users.noreply.github.com>
2024-10-05 21:06:52 +0530

    nm,

--- server/controllers/payments.js
+++ server/controllers/payments.js
@@ -4,9 +4,7 @@ const crypto = require("crypto")
 const User = require("../models/User")
 const mailSender = require("../utils/mailSender")
 const mongoose = require("mongoose")
-const {
-  courseEnrollmentEmail,
-} = require("../mail/templates/courseEnrollmentEmail")
+const {courseEnrollmentEmail} = require("../mail/templates/courseEnrollmentEmail")
 const { paymentSuccessEmail } = require("../mail/templates/paymentSuccessEmail")
 const CourseProgress = require("../models/CourseProgress")
 
