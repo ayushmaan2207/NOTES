2df5ef528daaeca8093f2b7075f974ca22c3177f
Ayushmaan <118063648+ayushmaan2207@users.noreply.github.com>
2024-10-22 20:52:30 +0530

    b

--- 3RD YEAR/JAVA/tempCodeRunnerFile.java
+++
@@ -1,6 +0,0 @@
-// MyThreadRunnable1 tR1 = new MyThreadRunnable1();
-        // Thread t1R = new Thread(tR1);
-        // MyThreadRunnable2 tR2 = new MyThreadRunnable2();
-        // Thread t2R = new Thread(tR2);
-        // t1R.start();
-        // t2R.start();
